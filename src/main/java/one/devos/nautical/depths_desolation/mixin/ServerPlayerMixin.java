8cb9cf3f3422414f913f638119baf6bae41bcf5f
TropheusJ <jverrellijr@icloud.com>
2023-10-12 17:35:45 -0400

    do not fudge, do not pass go, do not collect $200

--- src/main/java/one/devos/nautical/depths_desolation/mixin/ServerPlayerMixin.java
+++ src/main/java/one/devos/nautical/depths_desolation/mixin/ServerPlayerMixin.java
@@ -1,22 +1,32 @@
 package one.devos.nautical.depths_desolation.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+import com.mojang.authlib.GameProfile;
+
+import one.devos.nautical.depths_desolation.content.DdWorldgen;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
-public class ServerPlayerMixin {
-	@ModifyExpressionValue(
-			method = "fudgeSpawnLocation",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/world/level/dimension/DimensionType;hasSkyLight()Z"
-			)
-	)
-	private boolean dontFudge(boolean skylight) {
-		return false;
+public abstract class ServerPlayerMixin extends Player {
+	public ServerPlayerMixin(Level world, BlockPos pos, float yaw, GameProfile gameProfile) {
+		super(world, pos, yaw, gameProfile);
+	}
+
+	@Inject(method = "fudgeSpawnLocation", at = @At("HEAD"), cancellable = true)
+	private void dontFudge(ServerLevel world, CallbackInfo ci) {
+		if (DdWorldgen.isOverworld(world)) {
+			BlockPos pos = world.getSharedSpawnPos();
+			this.moveTo(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
+			ci.cancel();
+		}
 	}
 }
