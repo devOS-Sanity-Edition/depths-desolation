7a39e0eb67584e248044f8b2b508a6f47a48eb3b
TropheusJ <jverrellijr@icloud.com>
2023-10-12 13:27:25 -0400

    remove that for now

--- src/main/java/one/devos/nautical/depths_desolation/content/worldgen/feature/spawncave/SpawnCaveFeatureConfiguration.java
+++ src/main/java/one/devos/nautical/depths_desolation/content/worldgen/feature/spawncave/SpawnCaveFeatureConfiguration.java
@@ -6,11 +6,10 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.GeodeConfiguration;
 
-public record SpawnCaveFeatureConfiguration(boolean bonusChest, GeodeConfiguration geodeConfig) implements FeatureConfiguration {
+public record SpawnCaveFeatureConfiguration(boolean bonusChest) implements FeatureConfiguration {
 	public static final Codec<SpawnCaveFeatureConfiguration> CODEC = RecordCodecBuilder.create(
 			instance -> instance.group(
-					Codec.BOOL.fieldOf("bonus_chest").forGetter(SpawnCaveFeatureConfiguration::bonusChest),
-					GeodeConfiguration.CODEC.fieldOf("geode_config").forGetter(SpawnCaveFeatureConfiguration::geodeConfig)
+					Codec.BOOL.fieldOf("bonus_chest").forGetter(SpawnCaveFeatureConfiguration::bonusChest)
 			).apply(instance, SpawnCaveFeatureConfiguration::new)
 	);
 }
