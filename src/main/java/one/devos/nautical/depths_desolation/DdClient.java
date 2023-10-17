8479880999bc9ee4cc5906de4e6431377c3421b5
TropheusJ <jverrellijr@icloud.com>
2023-10-17 11:50:49 -0400

    full grass visuals

--- src/main/java/one/devos/nautical/depths_desolation/DdClient.java
+++ src/main/java/one/devos/nautical/depths_desolation/DdClient.java
@@ -1,6 +1,13 @@
 package one.devos.nautical.depths_desolation;
 
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+
+import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.world.level.GrassColor;
+
 import one.devos.nautical.depths_desolation.client.FogManager;
+
+import one.devos.nautical.depths_desolation.content.DdBlocks;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
@@ -10,5 +17,11 @@ public class DdClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
 		ClientTickEvents.START.register(FogManager::tick);
+		ColorProviderRegistry.BLOCK.register(
+				(state, level, pos, tintIndex) -> level != null && pos != null
+						? BiomeColors.getAverageGrassColor(level, pos)
+						: GrassColor.getDefaultColor(),
+				DdBlocks.FULL_GRASS
+		);
 	}
 }
