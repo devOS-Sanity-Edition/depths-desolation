bfeb1fb5fd6ba92e8eced6359e3c01b84dd7d855
TropheusJ <jverrellijr@icloud.com>
2023-10-24 11:31:11 -0400

    only spawn things that won't die immediately

---
+++ src/main/java/one/devos/nautical/depths_desolation/mixin/NaturalSpawnerMixin.java
@@ -0,0 +1,37 @@
+package one.devos.nautical.depths_desolation.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.NaturalSpawner;
+
+import one.devos.nautical.depths_desolation.content.DdWorldgen;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(NaturalSpawner.class)
+public class NaturalSpawnerMixin {
+	@Inject(
+			method = "isSpawnPositionOk",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/core/BlockPos;above()Lnet/minecraft/core/BlockPos;"
+			),
+			cancellable = true
+	)
+	private static void filterOverworldSurfaceSpawns(SpawnPlacements.Type location, LevelReader world, BlockPos pos,
+													 EntityType<?> entityType, CallbackInfoReturnable<Boolean> cir) {
+		if (DdWorldgen.isOverworld(world)) {
+			if (!entityType.is(EntityTypeTags.FREEZE_IMMUNE_ENTITY_TYPES)) {
+				if (world.canSeeSky(pos)) {
+					cir.setReturnValue(false);
+				}
+			}
+		}
+	}
+}
